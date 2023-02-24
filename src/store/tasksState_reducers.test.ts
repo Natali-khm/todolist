1e1002ffc7da20def2ff4b11575923f872fb8cb6
Nataliya Khamitsevich <nataliyakham@gmail.com>
2023-02-25 00:05:17 +0300

    tests for tasks state reducer has been completed

--- src/store/tasksState_reducers.test.ts
+++ src/store/tasksState_reducers.test.ts
@@ -1,6 +1,7 @@
 import { v1 } from "uuid";
 import { TasksStateType } from "../App";
 import { AddTaskAC, ChangeTaskStatusAC, ChangeTaskTitleAC, RemoveTaskAC, tasksStateReducer } from "./tasksState_reducers";
+import { AddTodoListAC, RemoveTodoListAC } from "./todolists_reducers";
 
 test('correct task should be added', () => {
     const todolistId1 = v1();
@@ -102,5 +103,55 @@ test('correct task should change its status', () => {
 
     expect(endState[todolistId2][0].isDone).toBe(false)
     expect(endState[todolistId2][1].isDone).toBe(true)
+
+})
+
+test('new task state should be created', () => {
+    const todolistId1 = v1();
+    const todolistId2 = v1();
+    const todolistId3 = v1();
+
+    const newTodoListTitle = 'Not forget'
+
+    const startState: TasksStateType = {
+
+        [todolistId1]:[
+            {id: v1(), title: "HTML&CSS", isDone: true},
+            {id: v1(), title: "JS", isDone: true},
+        ],
+        [todolistId2]:[
+            {id: v1(), title: "HTML&CSS2", isDone: true},
+            {id: v1(), title: "JS2", isDone: true},
+        ]        
+    }
+
+    const endState = tasksStateReducer(startState, AddTodoListAC(todolistId3, newTodoListTitle))
+
+    expect(endState[todolistId3].length).toBe(0)
+    expect(Object.keys(endState).length).toBe(3)
+    expect(endState.hasOwnProperty(todolistId3)).toBe(true)
+
+})
+
+test('correct tasks state should be deleted', () => {
+    const todolistId1 = v1();
+    const todolistId2 = v1();
+
+    const startState: TasksStateType = {
+
+        [todolistId1]:[
+            {id: v1(), title: "HTML&CSS", isDone: true},
+            {id: v1(), title: "JS", isDone: true},
+        ],
+        [todolistId2]:[
+            {id: v1(), title: "HTML&CSS2", isDone: true},
+            {id: v1(), title: "JS2", isDone: true},
+        ]        
+    }
+
+    const endState = tasksStateReducer(startState, RemoveTodoListAC(todolistId1))
+
+    expect(endState.hasOwnProperty(todolistId1)).toBe(false)
+    expect(endState.hasOwnProperty(todolistId2)).toBe(true)
 
 })
